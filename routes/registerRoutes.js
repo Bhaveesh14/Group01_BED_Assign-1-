a5b2698ece8ebdd6416f7639e7127a86ff9c3b60
Kavin-Balaji-NP-IT <kkavinbalaji200708@gmail.com>
2025-07-13 23:51:38 +0800

    Delete routes/registerRoutes.js

--- routes/registerRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const RegisterController = require('../controllers/RegisterController');
-const validateRegister = require('../validators/registerValidator');
-
-router.post('/', validateRegister, RegisterController.register);
-
-module.exports = router;
